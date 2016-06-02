13cb7a4109e680085b1e4c6d2887f71a264c590b
Chris Meyer <cmeyer1969@gmail.com>
2016-06-02 10:07:41 -0700

    Remove unused files and classes.
    Broadcaster, Unicode, ListModel are unused.
    
    Selection is moved to nionui.

--- nion/utils/Model.py
+++ nion/utils/Model.py
@@ -2,65 +2,14 @@
     Model classes. Useful for bindings.
 """
 
-# futures
-from __future__ import absolute_import
-
 # standard libraries
-import collections
-import copy
-import weakref
+# none
 
 # third party libraries
 # none
 
 # local libraries
 from . import Observable
-
-
-class ListModel(collections.MutableSequence, Observable.Observable):
-
-    """
-        Observable list.
-
-        Items added to this list are NOT recursively observed.
-    """
-
-    def __init__(self, parent, relationship_name):
-        super(ListModel, self).__init__()
-        self.__list = list()
-        self.__weak_parent = weakref.ref(parent)
-        self.__relationship_name = relationship_name
-
-    def __copy__(self):
-        return copy.copy(self.__list)
-
-    def __len__(self):
-        return len(self.__list)
-
-    def __getitem__(self, index):
-        return self.__list[index]
-
-    def __setitem__(self, index, value):
-        raise IndexError()
-
-    def __delitem__(self, index):
-        # get value
-        value = self.__list[index]
-        # do actual removal
-        del self.__list[index]
-        # keep storage up-to-date
-        self.__weak_parent().notify_remove_item(self.__relationship_name, value, index)
-
-    def __iter__(self):
-        return iter(self.__list)
-
-    def insert(self, index, value):
-        assert value not in self.__list
-        assert index <= len(self.__list) and index >= 0
-        # insert in internal list
-        self.__list.insert(index, value)
-        # keep storage up-to-date
-        self.__weak_parent().notify_insert_item(self.__relationship_name, value, index)
 
 
 class PropertyModel(Observable.Observable):

d5aa37230a3cdea2133ff36761f53f69c51763f8
Chris Meyer <cmeyer1969@gmail.com>
2016-11-15 11:46:20 -0800

    Continue preparation for distribution on PyPI.

--- setup.py
+++ setup.py
@@ -4,13 +4,21 @@ import setuptools
 import os
 
 setuptools.setup(
-    name="nion.utils",
-    version="0.1.0",
+    name="nionutils",
+    version="0.0.1",
+    author="Nion Software",
+    author_email="swift@nion.com",
+    description="Nion utility classes.",
+    long_description=open("README.rst").read(),
+    license="MIT",
+    url="https://github.com/nion-software/nionutils",
     packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=['numpy'],
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha"
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
     ],
     include_package_data=True,
-    test_suite="nion.utils.test"
+    test_suite="nion.utils.test",
 )

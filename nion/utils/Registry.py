ae312e8439581501f28c45b84114927e7d6cff5f
Chris Meyer <cmeyer1969@gmail.com>
2017-07-27 08:39:21 -0700

    Simplify Registry class (only tracks component type list now).

--- nion/utils/Registry.py
+++ nion/utils/Registry.py
@@ -3,9 +3,7 @@ An registry for objects by id, uuid, type.
 """
 
 # standard libraries
-import abc
 import typing
-import uuid
 
 # local libraries
 from . import Event
@@ -25,89 +23,52 @@ class Singleton(type):
         return cls.instance
 
 
-class Component(abc.ABC):
-    """Components must implement these read-only properties."""
-
-    @property
-    @abc.abstractmethod
-    def component_types(self) -> typing.Set[str]:
-        """The list of component types."""
-        ...
-
-    @property
-    @abc.abstractmethod
-    def component_id(self) -> str:
-        """The component id, must be a unique identifier starting with letter comprised of letter, numbers, underscores."""
-        ...
-
-    @property
-    @abc.abstractmethod
-    def component_uuid(self) -> uuid.UUID:
-        """The component uuid, must be unique."""
-        ...
-
-
 class ComponentManager(metaclass=Singleton):
     """The ComponentManager is a Singleton that implements the registry."""
     def __init__(self):
-        self.__component_by_id = dict()
+        self.__component_types = dict()
         self.__components_by_type = dict()
-        self.__component_by_uuid = dict()
         self.component_registered_event = Event.Event()
         self.component_unregistered_event = Event.Event()
 
-    def get_component(self, *, component_type: str=None, component_id: str=None, component_uuid: uuid.UUID=None, default=None) -> Component:
-        if component_id:
-            return self.__component_by_id.get(component_id, default)
-        if component_type:
-            components = self.__components_by_type.get(component_type)
-            if components and len(components) == 1:
-                return components[0]
-        if component_uuid:
-            return self.__component_by_uuid.get(component_uuid, default)
-        return default
-
-    def get_components_by_type(self, component_type:str) -> typing.Set[Component]:
+    def get_components_by_type(self, component_type:str) -> typing.Set[typing.Any]:
         return self.__components_by_type.get(component_type, list())
 
-    def register(self, component: Component) -> None:
-        self.__component_by_id[component.component_id] = component
-        self.__component_by_uuid[component.component_uuid] = component
-        for component_type in component.component_types:
+    def register(self, component, component_types: typing.Set[str]) -> None:
+        assert component not in self.__component_types
+        for component_type in component_types:
             component_set = self.__components_by_type.setdefault(component_type, set())
             component_set.add(component)
-        self.component_registered_event.fire(component)
+        self.__component_types[component] = component_types
+        self.component_registered_event.fire(component, component_types)
 
-    def unregister(self, component: Component) -> None:
-        del self.__component_by_id[component.component_id]
-        del self.__component_by_uuid[component.component_uuid]
-        for component_type in component.component_types:
+    def unregister(self, component) -> None:
+        assert component in self.__component_types
+        for component_type in self.__component_types[component]:
             self.__components_by_type.get(component_type).remove(component)
-        self.component_unregistered_event.fire(component)
+        component_types = self.__component_types.pop(component)
+        self.component_unregistered_event.fire(component, component_types)
 
 
-def get_component(*, component_type: str=None, component_id: str=None, component_uuid: uuid.UUID=None, default=None) -> Component:
-    """Returns component matching component_type, component_id, component_uuid or a default value if none is found.
-    
-    A component matching component_type will only be returned if it is unique. Otherwise use get_components_by_type.
-    """
-    return ComponentManager().get_component(component_type=component_type, component_id=component_id, component_uuid=component_uuid, default=default)
-
-def get_components_by_type(type: str) -> typing.Set[Component]:
+def get_components_by_type(component_type: str) -> typing.Set[typing.Any]:
     """Returns set of components matching component_type."""
-    return ComponentManager().get_components_by_type(type)
+    return ComponentManager().get_components_by_type(component_type)
 
-def register_component(component: Component) -> None:
-    """Register a component. This will trigger a component_registered_event. The component id and uuid must be unique."""
-    ComponentManager().register(component)
 
-def unregister_component(component: Component) -> None:
+def register_component(component, component_types: typing.Set[str]) -> None:
+    """Register a component and associated it with the set of types. This will trigger a component_registered_event."""
+    ComponentManager().register(component, component_types)
+
+
+def unregister_component(component) -> None:
     """Unregister a component. This will trigger a component_unregistered_event. The component must have been previously registered."""
     ComponentManager().unregister(component)
+
 
 def listen_component_registered_event(listener_fn) -> Event.EventListener:
     """Add a listener for the component registered event."""
     return ComponentManager().component_registered_event.listen(listener_fn)
+
 
 def listen_component_unregistered_event(listener_fn) -> Event.EventListener:
     """Add a listener for the component unregistered event."""

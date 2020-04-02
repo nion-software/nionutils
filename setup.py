89746318eca0225eeda6be6e1c90f7cf20dae9f8
Chris Meyer <cmeyer1969@gmail.com>
2020-04-02 16:57:30 -0700

    Switch to declarative setup.cfg + pyproject file.

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
-# -*- coding: utf-8 -*-
+from setuptools import setup
 
-import setuptools
-import os
-
-setuptools.setup(
-    name="nionutils",
-    version="0.3.20",
-    author="Nion Software",
-    author_email="swift@nion.com",
-    description="Nion utility classes.",
-    long_description=open("README.rst").read(),
-    url="https://github.com/nion-software/nionutils",
-    packages=["nion.utils", "nion.utils.test"],
-    license='Apache 2.0',
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.6",
-    ],
-    include_package_data=True,
-    test_suite="nion.utils.test",
-)
+if __name__ == "__main__":
+    setup()

c5d6a8c1be213b0cddf9a32aa15733b892b1c0b7
Chris Meyer <cmeyer1969@gmail.com>
2020-04-16 14:14:56 -0700

    Add added/discarded notifications to Observable for set-like behavior.

--- nion/utils/Observable.py
+++ nion/utils/Observable.py
@@ -22,22 +22,30 @@ class Observable:
         self.item_cleared_event = Event.Event()
         self.item_inserted_event = Event.Event()
         self.item_removed_event = Event.Event()
+        self.item_added_event = Event.Event()
+        self.item_discarded_event = Event.Event()
         self.item_content_changed_event = Event.Event()
 
     def notify_property_changed(self, key: str) -> None:
         self.property_changed_event.fire(key)
 
-    def notify_set_item(self, key, item):
+    def notify_set_item(self, key, item) -> None:
         self.item_set_event.fire(key, item)
 
-    def notify_clear_item(self, key):
+    def notify_clear_item(self, key) -> None:
         self.item_cleared_event.fire(key)
 
-    def notify_insert_item(self, key, value, before_index):
+    def notify_insert_item(self, key, value, before_index) -> None:
         self.item_inserted_event.fire(key, value, before_index)
 
-    def notify_remove_item(self, key, value, index):
+    def notify_remove_item(self, key, value, index) -> None:
         self.item_removed_event.fire(key, value, index)
 
-    def notify_item_content_changed(self, key, value, index):
+    def notify_add_item(self, key, value) -> None:
+        self.item_added_event.fire(key, value)
+
+    def notify_discard_item(self, key, value) -> None:
+        self.item_discarded_event.fire(key, value)
+
+    def notify_item_content_changed(self, key, value, index) -> None:
         self.item_content_changed_event.fire(key, value, index)

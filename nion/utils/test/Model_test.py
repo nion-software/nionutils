0968ee054386d0f902ba339104a5a1389b4094df
Chris Meyer <34664+cmeyer@users.noreply.github.com>
2025-03-28 14:27:25 -0700

    Minor standardization of event loop testing.

--- nion/utils/test/Model_test.py
+++ nion/utils/test/Model_test.py
@@ -17,6 +17,7 @@ from nion.utils import Stream
 @contextlib.contextmanager
 def event_loop_context() -> typing.Iterator[asyncio.AbstractEventLoop]:
     event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
     yield event_loop
     event_loop.stop()
     event_loop.run_forever()
